ecaa7792c49f2c130d8d1352e63480647a12d85e
Cel Pynenborg <celpynenborg@gmail.com>
2018-11-19 13:47:38 +0100

    added navbar with limited router support

--- frontend/src/app/sharedModules/ng-bootstrap.module.ts
+++ frontend/src/app/sharedModules/ng-bootstrap.module.ts
@@ -1,14 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule,
+          NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgbAlertModule.forRoot()
+    NgbAlertModule.forRoot(),
+    NgbCollapseModule.forRoot()
   ],
   exports: [
-    NgbAlertModule
+    NgbAlertModule,
+    NgbCollapseModule
   ],
   declarations: []
 })
