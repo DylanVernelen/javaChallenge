9979c47d6f93d9f2b87ff4dc62531cbe7d0ac430
sander <sander.philipsen@hotmail.com>
2018-11-19 11:31:31 +0100

    update routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AuthGuardService as AuthGuard } from './guards/auth-guard.service';
+import { RoleGuardService as RoleGuard } from './guards/role-guard.service';
 const routes: Routes = [
-  //  {path: '', component: , canActivate: [AuthGuard]}
-    //  {path: '', component: , isAdmin: [AuthGuard]}
+
+ /* Om te kijken of gebruiker is ingelogd
+   { path: '', component: ProfileComponent, canActivate: [AuthGuard] },
+
+    /* Om te kijken of gebruiker administrator is
+    {
+        path: 'admin',
+        component: AdminComponent,
+        canActivate: [RoleGuard],
+        data: {
+            expectedRole: 'admin'
+        }
+    }*/
 ];
 
 @NgModule({
