823cbfde307de2ca51e9ff332e0dda76325c7ce5
Tom <r0670352@student.thomasmore.be>
2018-11-19 10:41:04 +0100

    Backend: reward get,create,delete,all

---
+++ backend/api_routes.js
@@ -0,0 +1,72 @@
+// more routes for our API will happen here
+
+
+module.exports = 
+{
+	init: function(router, models)
+	{
+
+		// REWARD CREATE - POST
+		router.route('/reward/create').post(function(req, res)
+		{
+			var reward = new models.modelReward();
+
+			reward.rewardName = req.body.name;
+			reward.rewardOwner = req.body.owner;
+			reward.rewardWorth = req.body.worth;
+
+
+			reward.save(function(err)
+			{
+				if(err)
+					res.send(err);
+				
+				res.json({succes: true});
+			})
+		});
+
+
+		// REWARD GET BY ID - GET 
+		router.route('/reward/get/:reward_id').get(function(req, res)
+		{	
+			var model = models.modelReward;
+
+			model.findById(req.params.reward_id, function(err, reward)
+			{
+				if(err)
+					res.send(err)
+
+				res.json(reward);
+			})
+		});
+
+
+		router.route('/reward/delete/:reward_id').delete(function(req, res)
+		{
+			var model = models.modelReward;
+
+			model.remove({
+				_id: req.params.reward_id
+			}, function(err, reward)
+			{
+				if(err)
+					res.send(err);
+
+				res.json({succes: true});
+			});
+		});
+
+		router.route('/reward/all').get(function(req, res)
+		{
+			var model = models.modelReward;
+			model.find({}, function(err, rewards)
+			{
+				res.json(rewards);
+			})
+		})
+
+	}
+
+
+}
+
--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,65 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// BASE SETUP
+// =============================================================================
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Database
+var mongoose = require('mongoose');
+
+var modelReward = require('./models/reward.js');
+
+
+
+mongoose.connect('mongodb://root:ThomasMore1@ds055762.mlab.com:55762/rewardsystem', {useNewUrlParser: true}); // connect to our database
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+
+router.use(function(req, res, next) {
+    // do logging
+    console.log('API: routing new query');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+
+    res.json({ message: 'iAdviseRewardSystem: main content' });   
+});
+
+
+
+var models = 
+{
+	modelReward
+}
+
+
+var apiRoutes = require('./api_routes.js');
+apiRoutes.init(router, models);
+
+
+
+
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Starting server on port: ' + port);
