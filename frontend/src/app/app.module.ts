140f05a4f77b809408a8dc24dbe4ab1b381373a7
DylanVernelen <dylanver@live.be>
2018-11-19 11:09:37 +0100

    start mange users

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgBootstrapModule } from './sharedModules/ng-bootstrap.module';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
+import { UserTemComponent } from './manage-users/user-tem/user-tem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    ManageUsersComponent,
+    UserTemComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/manage-users/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CommonModule} from "@angular/common";
+
+import { FormsModule } from '@angular/forms';
+import { UserItemComponent } from './user-item/user-item.component';
+import {ManageUsersService} from "../services/manage-users.service";
+import {ManageUsersComponent} from "./manage-users.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [ManageUsersComponent, UserItemComponent],
+  providers: [ManageUsersService]
+})
+export class AppModule { }
