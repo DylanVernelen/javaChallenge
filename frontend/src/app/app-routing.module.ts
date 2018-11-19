39f9aa7c5201759d9e3c5849f4c8b171e7a01e35
sander <sander.philipsen@hotmail.com>
2018-11-19 11:09:53 +0100

    created authentication service

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //  {path: '', component: , canActivate: [AuthGuard]}
+    //  {path: '', component: , isAdmin: [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
