1bd3dea65574f24fef868d322f2993976d5007ea
sander <sander.philipsen@hotmail.com>
2018-11-19 15:26:18 +0100

    update header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,43 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgBootstrapModule } from './sharedModules/ng-bootstrap.module';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
-
 import { StoreModule } from './store/store.module';
-
 import { UserItemComponent } from './manage-users/user-item/user-item.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-
+import {HttpModule} from "@angular/http";
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NavbarComponent,
     ManageUsersComponent,
-
+      UserItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgBootstrapModule,
-
-
+      StoreModule,
+     HttpClientModule,
+      FormsModule
 
   ],
   providers: [
 
 
-    StoreModule,
-    FormsModule,
-    HttpClientModule
 
   ],
   bootstrap: [AppComponent]
