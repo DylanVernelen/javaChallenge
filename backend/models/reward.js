fa10c29bb4601e6b4ab3108b07bb3da160045152
Cel Pynenborg <celpynenborg@gmail.com>
2018-11-19 11:40:08 +0100

    re-added reward.js because git works in mysterious ways

---
+++ backend/models/reward.js
@@ -0,0 +1,10 @@
+// app/models/reward.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema 
+ var RewardSchema = new Schema(
+{
+	rewardName: String,
+	rewardOwner: String,
+	rewardWorth: Number,
+});
+ module.exports = mongoose.model('Reward', RewardSchema);
