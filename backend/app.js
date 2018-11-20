61d1320dc6482e94630aa77b7cb7b9a41197d669
Tom <r0670352@student.thomasmore.be>
2018-11-20 13:05:08 +0100

    cors

--- backend/app.js
+++ backend/app.js
@@ -64,9 +64,11 @@ router.use(function(req, res, next) {
   	res.header("Access-Control-Allow-Credentials", "true");
 */
 	//res.header("Access-Control-Allow-Headers", "Authorization");
+
+  	res.header("Access-Control-Allow-Credentials", "true");
  	res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if ('OPTIONS' == req.method)
     {
 	    res.sendStatus(200);
