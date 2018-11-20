837c442fe3012b74bde6c870e09e4ea4416630c9
Tom <r0670352@student.thomasmore.be>
2018-11-20 12:35:15 +0100

    cors

--- backend/app.js
+++ backend/app.js
@@ -60,9 +60,9 @@ router.use(function(req, res, next) {
 
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  	//res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  
-	res.header("Access-Control-Allow-Headers", "Authorization");
+	//res.header("Access-Control-Allow-Headers", "Authorization");
 
     console.log('API: routing new query: ' + req.url);
 
