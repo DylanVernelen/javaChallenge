2449c2c1d8afdad2dc3c28f5c91e4fe80b7ef3b3
Tom <r0670352@student.thomasmore.be>
2018-11-20 13:00:03 +0100

    cors

--- backend/app.js
+++ backend/app.js
@@ -48,14 +48,14 @@ var apiRoutes = require('./api_routes.js');
 
 
 router.use(function(req, res, next) {
-
+/*
 	if ('OPTIONS' == req.method) {
   		res.header('Access-Control-Allow-Origin', '*');
   		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   		res.send(200);
 	}
-
+*/
 
 
 	res.header("Access-Control-Allow-Origin", "*");
